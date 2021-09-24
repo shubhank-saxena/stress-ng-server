45c1174f9969cf6279693d66589d39d2bdc5cfeb
Shubahnk Saxena <saxena.shubhank.19@gmail.com>
2021-09-24 19:12:51 +0530

    Add for loop to accomodate multiple servers in cli tool

--- cli.py
+++ cli.py
@@ -1,16 +1,18 @@
 import os
 import requests
 
-
-BASE_URL = 'http://localhost:8000'
 print("Welcome to the stress-ng-cli tool!")
 
 input_file = input("Enter the location of the configuration file for stress-ng-cli : \n")
+server_lists = [int(x) for x in input("Enter the IP address of various servers").split()]
 
 files = {}
 files['file'] = open(f'{os.path.abspath(input_file)}', 'rb')
-response = requests.post(url=f'{BASE_URL}/v1/runtask/fileupload/', files=files)
-if response.status_code == 201:
-    print("Configuration file uploaded")
-else:
-    print(f'Error: {response.text}')
+
+for ip in server_list:
+    BASE_URL = f'http://{ip}:8700'
+    response = requests.post(url=f'{BASE_URL}/v1/runtask/fileupload/', files=files)
+    if response.status_code == 201:
+        print(f"Configuration file uploaded to {ip} server")
+    else:
+        print(f'Error: {response.text}')
--- server.py
+++ server.py
@@ -99,4 +99,4 @@ def postJSON():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8700, debug=True)
